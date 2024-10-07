55a0ba9c831909ebfa2227aa9d9a315299ea2041
Kevin <kfaucher@dtcc.edu>
2024-10-07 17:32:30 -0400

    first commit

---
+++ Demo 1/script.js
@@ -0,0 +1,18 @@
+// Original array
+const fruits = ['Apple', 'Banana', 'Cherry'];
+
+// Destructuring
+const [first, second] = fruits;
+
+console.log(first);  // Output: 'Apple'
+console.log(second); // Output: 'Banana'
+
+
+// Original object
+const person = { name: 'John', age: 30 };
+
+// Destructuring
+const { name, age } = person;
+
+console.log(name); // Output: 'John'
+console.log(age);  // Output: 30
